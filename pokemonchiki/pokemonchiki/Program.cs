2e4ac4a88ee6e084391c27f0786e994e2e16c7e3
CanadeTachi <148984895+CanadeTachi@users.noreply.github.com>
2024-09-24 15:15:24 +0400

    pokemon0
    pokemon 0

---
+++ pokemonchiki/pokemonchiki/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static pokemonchiki.pokemonchiki;
+
+namespace pokemonchiki
+{
+    class pokemonchiki
+    {
+        public string Name { get; set; }
+        public int HP { get; set; }
+        public int Attack { get; set; }
+        public int Defense { get; set; }
+        public int SpecialAttack { get; set; }
+        public int SpecialDefense { get; set; }
+        public int Speed { get; set; }
+
+
+        public pokemonchiki(string name, int hp, int attack, int defense, int specialAttack, int specialDefense, int speed, string type, string abilities, string nature)
+        {
+            Name = name;
+            HP = hp;
+            Attack = attack;
+            Defense = defense;
+            SpecialAttack = specialAttack;
+            SpecialDefense = specialDefense;
+            Speed = speed;
+
+        }
+
+        public virtual void PrintInfo()
+        {
+            Console.WriteLine($"Имя: {Name}");
+            Console.WriteLine($"Очки здоровья: {HP}");
+            Console.WriteLine($"Атака: {Attack}");
+            Console.WriteLine($"Защита: {Defense}");
+            Console.WriteLine($"Специальная атака: {SpecialAttack}");
+            Console.WriteLine($"Специальная защита: {SpecialDefense}");
+            Console.WriteLine($"Скорость: {Speed}");
+        }
+
+    }
+}
